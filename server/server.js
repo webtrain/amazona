7ac2b84cb1ee99cff7e4e070a19d28c518661b42
webtrain <32815688+laszloh111@users.noreply.github.com>
2020-11-11 11:10:45 +0100

    add some changes

--- server/server.js
+++ server/server.js
@@ -14,10 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Server is ready.');
 });
-
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
